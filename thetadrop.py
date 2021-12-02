0691288f8e6b938559c3d5b13b93e0d91c0d1372
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-12-02 06:07:15 +0600

    Initial commit

---
+++ thetadrop.py
@@ -0,0 +1,37 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import pathlib
+import random
+
+# Setting the chrome_options
+global chrome_options
+chrome_options = Options()
+scriptDirectory = pathlib.Path().absolute()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--user-data-dir=chrome-data")
+chrome_options.add_argument('--profile-directory=Profile 8')
+prefs = {"profile.default_content_setting_values.notifications": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument('disable-infobars')
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument("user-data-dir=chrome-data")
+chrome_options.add_argument(f"user-data-dir={scriptDirectory}\\userdata")
+
+driver = webdriver.Chrome("K:\Project\Python\\thetadrop\chromedriver.exe", chrome_options=chrome_options)
+
+
+# print(input(" Connect your waller address :"))
+driver.implicitly_wait(10)
+driver.get("https://wpt.thetadrop.com/drop/drop_h3kf955wed8em15bv0s24s1d7jk")
+# driver.get("https://www.thetadrop.com/marketplace")
+# driver.get("https://play.cryptomines.app/")
+
+print(input("Start Project ..... :"))
+
+deposit_elements = driver.find_element_by_xpath("(//button[.='Deposit funds'])[2]")
+deposit_elements.click()
